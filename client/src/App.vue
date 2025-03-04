44b486e218df9a03f82896279ed5adef92f3469b
Eliseev Vadim <eliseevv02@mail.ru>
2025-03-04 23:30:16 +0300

    feat: add vuetify to project

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!"/>
-        </div>
-    </header>
-
-    <main>
-        <TheWelcome/>
-    </main>
+    <v-app>
+        <v-container>
+            <v-btn color="primary">Vuetify is here</v-btn>
+        </v-container>
+    </v-app>
 </template>
 
 <style scoped>
